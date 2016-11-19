cbadd596ce6318d47904549c10d84dbe3f0a5ca4
Mike Sigsworth <mikesigsworth@hotmail.com>
2016-11-18 22:14:28 -0700

    Displaying gif results

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+
+const GiphyApiUrl = 'http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Giphy Search';
+  gifs: any[];
+
+  performSearch(searchTerm: HTMLInputElement): void {
+    this._http
+      .get(GiphyApiUrl + searchTerm.value)
+      .subscribe(
+        (res: Response) => this.gifs = res.json().data,
+        error => this._handleError(error));
+  }
+
+  private _handleError(error) {
+    const errorMsg = error.json().error || 'Error ' + error;
+    console.log(errorMsg);
+  }
+
+  constructor(private _http: Http) { }
 }
